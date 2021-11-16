3b946ccca58bd9f30be4b175aea7079358cad444
绚丽彩虹 <flandrestudio.cn@gmail.com>
2021-11-16 10:33:39 +0800

    Emmmm....

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b0acb833197d9b01442af27df9a0f483c0be9fdc',
+        'name' => 'dark495/adminphp',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dark495/adminphp' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b0acb833197d9b01442af27df9a0f483c0be9fdc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
